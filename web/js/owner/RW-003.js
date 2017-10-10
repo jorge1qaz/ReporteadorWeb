cf7994e2b8395c1069a276c61f253e7d66358f51
jorgeluis <jorgealcantara5a@gmail.com>
2017-10-10 16:26:13 -0500

    headers, formulario ganancias y pérdidas

---
+++ web/js/owner/RW-003.js
@@ -0,0 +1,35 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawStacked);
+
+function drawStacked() {
+      var data = google.visualization.arrayToDataTable([
+        ['Mes', 'Acumulado'],
+        ['Enero,', 817500],
+        ['Febrero,', 379200],
+        ['Marzo,', 2695000],
+        ['Abril,', 2099000],
+        ['Mayo,', 1526000],
+        ['Junio,', 1526000],
+        ['Julio,', 817500],
+        ['Agosto,', 1645000],
+        ['Septiembre,', 1526000],
+        ['Octubre,', 485000],
+        ['Noviembre,', 1526000],
+        ['Diciembre,', 817500]
+      ]);
+
+      var options = {
+        title: 'Resultado anual de la empresa XXXXXXXX',
+        chartArea: {width: '50%'},
+        isStacked: true,
+        hAxis: {
+          title: 'Total en dólares',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'Meses'
+        }
+      };
+      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
--- web/js/owner/login.js
+++ web/js/owner/login.js
@@ -1,10 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var $blockContrasenia = $("#blockContrasenia");
     var $btnAcceder = $("#btnAcceder");
+    var $btnCerrarSesion = $("#btnCerrarSesion");
+    var $linkCambiarContrasenia = $("#linkCambiarContrasenia") 
     $blockContrasenia.hide();
-    $("#btnAcceder").on('click', function() {
+    $btnAcceder.on('click', function () {
         $("#blockCorreo").hide(1000);
         $blockContrasenia.show();
         $btnAcceder.text("Acceder");
     });
+    $linkCambiarContrasenia.hover(function () {
+        $(this).addClass("green-text");
+            }, function () {
+                    $(this).removeClass("green-text");
+                }      
+    );
+    $btnCerrarSesion.hover(function () {
+        $(this).addClass("green-text")
+                .css("cursor","pointer");
+            }, function () {
+                    $(this).removeClass("green-text");
+                }      
+    );
 });
