62ad6d3d205c9a08bb87383f4c45855cf58c7280
jorgeluis <jorgealcantara5a@gmail.com>
2017-10-13 09:34:28 -0500

    add table

--- web/js/owner/RW-003.js
+++ web/js/owner/RW-003.js
@@ -1,90 +1,7 @@
-/*google.charts.load('current', { packages: ['corechart', 'bar'] });
-google.charts.setOnLoadCallback(drawStacked);
-
-function drawStacked() {
-    var data = google.visualization.arrayToDataTable([
-        ['Mes', 'Acumulado'],
-        ['Enero', 817500],
-        ['Febrero', 379200],
-        ['Marzo', 2695000],
-        ['Abril', 2099000],
-        ['Mayo', 1526000],
-        ['Junio', 1526000],
-        /*['Julio', 817500],
-        ['Agosto', 1645000],
-        ['Septiembre', 1526000],
-        ['Octubre', 485000],
-        ['Noviembre', 1526000],
-        ['Diciembre', 817500]
-    ]);
-
-    var options = {
-        title: 'Resultado anual de la empresa XXXXXXXX',
-        chartArea:
-        {
-            width: '60%'
-        },
-        isStacked: true,
-
-        hAxis: {
-            title: 'Total',
-            minValue: 0,
-        },
-        vAxis: {
-            title: 'Meses'
-        }
-
-    };
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-
-    $(window).resize(function () {
-        if ($(window).width() < 481) //Celulares
-            $("#chart_div").hide();
-        else
-            if ($(window).width() < 769) //Tabletas    
-                $("#chart_div").show();
-            else // Pcs o laptops.
-                $("#chart_div").show();
-    });
-}*/
-
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(DibujarGraficoBarras);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Mes', 'Acumulado'],
-        ['Enero', 817500],
-        ['Febrero', 379200],
-        ['Marzo', 2695000],
-        ['Abril', 2099000],
-        ['Mayo', 1526000],
-        ['Junio', 1526000]
-        /*['Julio', 817500],
-        ['Agosto', 1645000],
-        ['Septiembre', 1526000],
-        ['Octubre', 485000],
-        ['Noviembre', 1526000],
-        ['Diciembre', 817500]*/
-    ]);
-    var options = {
-        title: 'Gráfico ',
-        hAxis: { title: 'Meses', titleTextStyle: { color: '#333' } },
-        chartArea:
-        {
-            width: '70%'
-        },
-        vAxis: { minValue: 0 }
-    };
-
-    var chart = new google.visualization.AreaChart(document.getElementById('chart_div2'));
-    chart.draw(data, options);
-}
-
-google.charts.load('current', {'packages':['bar']});
-google.charts.setOnLoadCallback(drawChart2);
-function drawChart2() {
+function DibujarGraficoBarras () {
     var data = google.visualization.arrayToDataTable([
         ['Mes', 'Total'],
         ['Enero', 817500],
@@ -101,15 +18,6 @@ function drawChart2() {
         ['Diciembre', 817500]
     ]);
 
-    var options = {
-        bars: 'horizontal', // Required for Material Bar Charts.
-        hAxis: { format: 'decimal' },
-        height: 400,
-        colors: ['rgb(33, 142, 202)', '#1b9e77', '#7570b3'],
-        bar: {groupWidth: "95%"},
-        legend: { position: "none" },
-    };
-
     var view = new google.visualization.DataView(data);
     view.setColumns([0, 1,
                      { calc: "stringify",
@@ -117,8 +25,18 @@ function drawChart2() {
                        type: "string",
                        role: "annotation" }
                      ]);
+    var options = {
+        bars: 'horizontal', // Required for Material Bar Charts.
+        hAxis: { format: 'decimal' },
+        height: 400,
+        colors: ['rgb(33, 142, 202)', '#1b9e77', '#7570b3'],
+        bar: {groupWidth: "85%"},
+        legend: { position: "none" },
+    };
 
-    var chart = new google.charts.Bar(document.getElementById('chart_div3'));
+    //var chart = new google.charts.Bar(document.getElementById('chart_div3'));
+    //chart.draw(data, google.charts.Bar.convertOptions(options));
 
-    chart.draw(data, google.charts.Bar.convertOptions(options));
+    var chart = new google.visualization.BarChart(document.getElementById("chart_div3"));
+    chart.draw(view, options);
 }
