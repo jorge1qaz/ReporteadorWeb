652f1b5d2734d661e2ef15ac3620981b81c698ae
jorgeluis <jorgealcantara5a@gmail.com>
2017-10-20 14:09:39 -0500

    actualización

--- web/js/owner/RW-004.js
+++ web/js/owner/RW-004.js
@@ -6,18 +6,48 @@ $(function(){
 
     });
 
-    $('#example').DataTable( {
+    $('#example2').DataTable( {
         //"ajax": "datos.json",
         "ajax":"../js/data/listaCuentas.json",
         "columns": [
-            /*{ "data": "name" },
-            { "data": "position" },
-            { "data": "office" },
-            { "data": "salary" },
-            { "data": "start_date" },
-            { "data": "extn" },*/
             { "data": "ccod_cue" },
             { "data": "cdsc" }
+        ],
+        "language": {
+            "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    } );
+    $('#example').DataTable( {
+        //"ajax": "datos.json",
+        "ajax":"../js/data/reporteCuentaspendientes.json",
+        "columns": [
+            { "data": "ccod_cli" },
+            { "data": "cmes" },
+            { "data": "debe" },
+            { "data": "haber" },
+            { "data": "total" },
+            { "data": "crazon" }
         ],
         "language": {
             "sProcessing":     "Procesando...",
