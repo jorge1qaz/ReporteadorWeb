dcb6b1c2238392363ebb06f22b5949bcd11e263c
jorgeluis <jorgealcantara5a@gmail.com>
2017-11-17 16:12:00 -0500

    responsividad

--- web/js/owner/RW-006.js
+++ web/js/owner/RW-006.js
@@ -12,12 +12,41 @@ if(dia < 10) {
     dia = '0' + dia;
 }
 var fechaCompleto = anio + "." + mes + "." + dia;
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+$("#lstTipoMoneda").on("change", function () {
+    listarReporte($("#lstMes").val(), $(this).val());
+});
+$("#lstMes").on("change", function () {
+    var textoMes = $("#lstMes option:selected").text();
+    $("#monthHeader").text(textoMes);
+    listarReporte($(this).val(), $("#lstTipoMoneda").val() );
+});
 
 $(document).ready(function() {
-    
-    
-    listarReporte();
+    $("#lstMes").val(mes).attr("selected");
+    $("#btnGenerarReporte").on("click", function() {
+        listarReporte($("#lstMes").val(), $("#lstTipoMoneda").val());
+    });
+    $("#monthHeader").text("");
+    $("#monthtitle").text("");
 });
+function CheckFile(urlFile) {
+    $.UrlExists = function (url) {
+        var http = new XMLHttpRequest();
+        http.open('HEAD', url, false);
+        http.send();
+        return http.status != 404;
+    }
+    if ($.UrlExists(urlFile)) {
+        return true;
+        console.log(urlFile);
+    } else {
+        return false;
+        console.log(urlFile);
+    }
+    
+}
 var idioma = {
     "sProcessing":     "Procesando...",
     "sLengthMenu":     "Mostrar _MENU_ registros",
@@ -40,75 +69,100 @@ var idioma = {
     "oAria": {
         "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
         "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-    }
+    },
+    buttons: {
+        copyTitle: 'Los datos fueron copiados',
+        copyInfo: {
+            _: 'Copiados %d filas al portapapeles',
+            1: 'Copiado 1 fila al portapapeles',
+    }}
 }
-var listarReporte = function() {
-    var tblReportes = $('#tablaReporte').DataTable( {
-        "destroy":true,
-        "processing": true,
-        responsive: true,
-        "ajax":"../js/data/" + idCliente + "/" + "rptsMrgTld/" + "ReporteDolares1.json",
-        "columns": [
-            { "data": "C" },
-            { "data": "D" },
-            { "data": "M" },
-            { "data": "U" },
-            { 
-                "data": "PV",
-                "render": function(precioVenta) {
-                    return "S/ " + precioVenta
-                }
-            },
-            { 
-                "data": "PC",
-                "render": function(precioCosto) {
-                    return "S/ " + precioCosto
-                }
-            },
-            { "data": "MUU" },
-            { "data": "PM" },
-            { 
-                "data": "MV",
-                "render": function(montoVentas) {
-                    return "S/ " + montoVentas
-                }
-            },
-            { 
-                "data": "MC",
-                "render": function(montoCosto) {
-                    return "S/ " + montoCosto
-                }
-            },
-            { 
-                "data": "MU",
-                "render": function(margenUtil) {
-                    return "S/ " + margenUtil
-                }
-            }
-        ],
-        "language": idioma,
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excelHtml5', {
-                extend: 'pdf',
-                text: 'PDF',
-                orientation: 'landscape',
-                title: 'Reporte:  Margen de utilidad por producto - ' + fechaCompleto,
-                filename: 'Margen de utilidad por producto ' + fechaCompleto,
-            }, {
-                extend: 'print',
-                text: 'Imprimir',
-                title: 'Reporte:  Margen de utilidad por producto',
-                exportOptions: {
-                    modifier: {
-                        page: 'current'
+var listarReporte = function(mesProcesamiento, tipoMoneda) {
+    var urlFileJson = "../js/data/" + idCliente + "/" + "rptsMrgTld/" + "Reporte" + tipoMoneda + mesProcesamiento + ".json";
+    var simboloMoneda = "";
+    if (CheckFile(urlFileJson)) {
+        if (tipoMoneda == "Soles") {
+            simboloMoneda = "S/ ";
+        } else {
+            simboloMoneda = "$ ";
+        }
+        var tblReportes = $('#tablaReporte').DataTable( {
+            "destroy":true,
+            "processing": true,
+            responsive: true,
+            "ajax":urlFileJson,
+            "columns": [
+                { "data": "C" },
+                { "data": "D" },
+                { "data": "M" },
+                { "data": "U" },
+                { 
+                    "data": "PV",
+                    "render": function(precioVenta) {
+                        return simboloMoneda + precioVenta
                     }
                 },
-            }
-        ],
-        "order": [[ 3, "desc" ]]
-    } );
-    $(".buttons-html5").addClass("btn btn-primary");
-    $(".buttons-print").addClass("btn btn-primary");
-    $(".buttons-copy span:first").text("Copiar");
+                { 
+                    "data": "PC",
+                    "render": function(precioCosto) {
+                        return simboloMoneda + precioCosto
+                    }
+                },
+                { "data": "MUU" },
+                { "data": "PM" },
+                { 
+                    "data": "MV",
+                    "render": function(montoVentas) {
+                        return simboloMoneda + montoVentas
+                    }
+                },
+                { 
+                    "data": "MC",
+                    "render": function(montoCosto) {
+                        return simboloMoneda + montoCosto
+                    }
+                },
+                { 
+                    "data": "MU",
+                    "render": function(margenUtil) {
+                        return simboloMoneda + margenUtil
+                    }
+                }
+            ],
+            "language": idioma,
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', {
+                    extend: 'excel',
+                    text: 'Excel',
+                    title: 'Reporte: Margen de utilidad por producto - Para el mes de ' + meses[mesProcesamiento - 1],
+                    filename: 'Margen de utilidad por producto ' + fechaCompleto,
+                }, {
+                    extend: 'pdf',
+                    text: 'PDF',
+                    orientation: 'landscape',
+                    title: 'Reporte: Margen de utilidad por producto - Para el mes de ' + meses[mesProcesamiento - 1],
+                    filename: 'Margen de utilidad por producto ' + fechaCompleto,
+                }, {
+                    extend: 'print',
+                    text: 'Imprimir',
+                    title: 'Reporte:  Margen de utilidad por producto',
+                    exportOptions: {
+                        modifier: {
+                            page: 'current'
+                        }
+                    },
+                }
+            ],
+            "order": [[ 3, "desc" ]]
+        } );
+        $(".buttons-html5").addClass("btn btn-primary");
+        $(".buttons-print").addClass("btn btn-primary");
+        $(".buttons-copy span:first").text("Copiar");
+        $("#monthtitle").text(" para el mes de ");
+    } else {
+        alert("Ningún dato para éste filtro.");
+        $("#monthtitle").text("");
+        $("#monthHeader").text("");
+    }
 }
